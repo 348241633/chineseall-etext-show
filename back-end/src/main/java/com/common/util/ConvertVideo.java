69aa0918e0e8af4479e00de9b83e9c3586fc1014
lego-jspx01 <88>
2018-08-28 09:30:26 +0800

    delete

--- back-end/src/main/java/com/common/util/ConvertVideo.java
+++
@@ -1,7 +0,0 @@
-package com.common.util;
-
-/**
- * Created by Admin on 18/8/13.
- */
-public class ConvertVideo {
-}
--- back-end/src/main/java/com/edusoft/sysmanage/service/DataSyncService.java
+++
@@ -1,8 +0,0 @@
-package com.edusoft.sysmanage.service;
-
-/**
- * Created by Admin on 18/8/15.
- */
-public interface DataSyncService {
-    void syncDictInfo();
-}
